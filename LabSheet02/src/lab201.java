0a9a78eda6598c305940d9517162edea62fd6954
NarapatLwn <thiscutterz@gmail.com>
2026-07-07 17:23:01 +0700

    Add files via upload

---
+++ LabSheet02/src/lab201.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class lab201 {
+
+	public static void main(String[] args) {
+		ArrayList<String > names = new ArrayList<>();
+		
+		names.add("Alice");
+		names.add("Emma");
+		names.add("Leon");
+		names.add("John");
+		
+		System.out.println("lenght = "+names.size());
+		System.out.println("all name in ArrayList = "+names);
+		
+
+	}
+
+}
